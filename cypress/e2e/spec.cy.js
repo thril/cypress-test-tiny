872e2c1f5c7ab24159fafdca2d27b6e11155b6ad
Philip Thompson <philip.thompson@skyspecs.com>
2024-01-10 12:14:35 -0600

    added tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+const url = "https://example.com";
+
+describe("page", () => {
+  it("fails", () => {
+    let count = 0;
+    cy.intercept("GET", url, (req) => {
+      count++;
+      console.info(`[${count}] requesting ${url}...`);
+      req.reply({ forceNetworkError: true });
+    }).as("stub");
+
+    cy.visit(url);
+
+    cy.wait("@stub");
+  });
+});
